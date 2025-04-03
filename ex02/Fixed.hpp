27094411f4967d96fc1745d500a0c610bd80363b
Hichem Abderrahmane Ali Mahdi <hali-mah@2-I-7.42heilbronn.de>
2025-04-03 11:36:33 +0200

    adding the Arethmetic operations in header part

--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 11:20:16 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/04/03 11:28:19 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/04/03 11:36:09 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,8 @@ class Fixed
     float   toFloat( void ) const;
     int     toInt( void ) const;
 
+
+    //Bool operations
     bool operator>(const Fixed& rhs) const;
     bool operator<(const Fixed& rhs) const;
     bool operator>=(const Fixed& rhs) const;
@@ -42,6 +44,14 @@ class Fixed
     bool operator==(const Fixed& rhs) const;
     bool operator!=(const Fixed& rhs) const;
 
+    //Arethmetic operations
+    Fixed operator+(const Fixed& rhs);
+    Fixed operator-(const Fixed& rhs);
+    Fixed operator*(const Fixed& rhs);
+    Fixed operator/(const Fixed& rhs);
+
+    //Incrementation decrementation operators
+    
     friend std::ostream& operator<<(std::ostream& os, const Fixed& fixed);    
 };
 
